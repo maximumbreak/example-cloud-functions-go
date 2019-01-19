70ac307c640e97bd0b1d7a54309e0eaed43ec9b6
first <firstnaruto@msn.com>
2019-01-19 14:27:35 +0700

    [✨] Use go module

--- hello/function.go
+++ hello/function.go
@@ -4,16 +4,24 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type Result struct {
 	Code    int
 	Message string
+	Token   string
 }
 
 type Body struct {
 	Message string
 }
+
+const (
+	mySigningKey = "BeforeSecond-Golang"
+)
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	data, _ := ioutil.ReadAll(r.Body)
@@ -24,12 +32,31 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(Result{
 			500,
 			"Internal Server Error",
+			"",
 		})
+		return
 
 	} else {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"Message": body.Message,
+			"exp":     time.Now().Add(time.Hour * 72).Unix(),
+		})
+		tokenString, err := token.SignedString([]byte(mySigningKey))
+
+		if err != nil {
+			json.NewEncoder(w).Encode(Result{
+				500,
+				err.Error(),
+				"",
+			})
+			return
+		}
+
 		json.NewEncoder(w).Encode(Result{
 			200,
 			body.Message,
+			tokenString,
 		})
+		return
 	}
 }
---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module go-cloud-functions-test/hello
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
